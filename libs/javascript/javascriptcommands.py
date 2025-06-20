da2b1dada6edba1e8f0b134c0bca87359c6288b8
pensta-security <pensta.org@outlook.com>
2025-06-20 22:35:20 +0100

    Extract walk functions JS

--- libs/javascript/javascriptcommands.py
+++ libs/javascript/javascriptcommands.py
@@ -9,6 +9,7 @@ class JavascriptCommands:
         self.driver = webdriver
         self.jsinjector = jsinjector
         self.logger = logger or FileLogger()
+        self.jsinjector.add_js_file('libs/javascript/js/walk_functions.js')
         
     def search_for_urls(self):
         self.jsinjector.execute_javascript(self.driver, 'wn_findStringsWithUrls();')
@@ -18,42 +19,7 @@ class JavascriptCommands:
 
 
     def walk_functions(self):
-        #javascript="jsproberesults=[];for (name in this) {  try{jsproberesults.push( {'name':''+name, 'value': ''+this[name]})}catch(err){var anyerror='ignore'};};return jsproberesults"
-        javascript = """
-window.wn_walk_functions = function(rootnode, pathstring){
-    var blacklist = ["InstallTrigger", "window", "__webDriverComplete", "__webDriverArguments", "close","stop","focus","blur","open","alert","confirm","prompt","print","postMessage","captureEvents","releaseEvents","getSelection","getComputedStyle","matchMedia","moveTo","moveBy","resizeTo","resizeBy","scroll","scrollTo","scrollBy","requestAnimationFrame","cancelAnimationFrame","getDefaultComputedStyle","scrollByLines","scrollByPages","sizeToContent","updateCommands","find","dump","setResizable","requestIdleCallback","cancelIdleCallback","btoa","atob","setTimeout","clearTimeout","setInterval","clearInterval","createImageBitmap","fetch","self","name","history","locationbar","menubar","personalbar","scrollbars","statusbar","toolbar","status","closed","frames","length","opener","parent","frameElement","navigator","external","applicationCache","screen","innerWidth","innerHeight","scrollX","pageXOffset","scrollY","pageYOffset","screenX","screenY","outerWidth","outerHeight","performance","mozInnerScreenX","mozInnerScreenY","devicePixelRatio","scrollMaxX","scrollMaxY","fullScreen","mozPaintCount","ondevicemotion","ondeviceorientation","onabsolutedeviceorientation","ondeviceproximity","onuserproximity","ondevicelight","sidebar","crypto","onabort","onblur","onfocus","onauxclick","oncanplay","oncanplaythrough","onchange","onclick","onclose","oncontextmenu","ondblclick","ondrag","ondragend","ondragenter","ondragexit","ondragleave","ondragover","ondragstart","ondrop","ondurationchange","onemptied","onended","oninput","oninvalid","onkeydown","onkeypress","onkeyup","onload","onloadeddata","onloadedmetadata","onloadend","onloadstart","onmousedown","onmouseenter","onmouseleave","onmousemove","onmouseout","onmouseover","onmouseup","onwheel","onpause","onplay","onplaying","onprogress","onratechange","onreset","onresize","onscroll","onseeked","onseeking","onselect","onshow","onstalled","onsubmit","onsuspend","ontimeupdate","onvolumechange","onwaiting","onselectstart","ontoggle","onpointercancel","onpointerdown","onpointerup","onpointermove","onpointerout","onpointerover","onpointerenter","onpointerleave","ongotpointercapture","onlostpointercapture","onmozfullscreenchange","onmozfullscreenerror","onanimationcancel","onanimationend","onanimationiteration","onanimationstart","ontransitioncancel","ontransitionend","ontransitionrun","ontransitionstart","onwebkitanimationend","onwebkitanimationiteration","onwebkitanimationstart","onwebkittransitionend","onerror","speechSynthesis","onafterprint","onbeforeprint","onbeforeunload","onhashchange","onlanguagechange","onmessage","onmessageerror","onoffline","ononline","onpagehide","onpageshow","onpopstate","onstorage","onunload","localStorage","origin","isSecureContext","indexedDB","caches","sessionStorage","document","location","top","addEventListener","removeEventListener","dispatchEvent"];
-    var rtndata = [];
-    for (name in rootnode) {  
-        typefound = typeof rootnode[name];
-        var fullname = ""+pathstring+"."+name;
-        if(typefound == "object"){
-            blacklistlookup = fullname.substring(5);
-            if (blacklist.includes(blacklistlookup) == false ) {
-                console.log("OBJECT: "+fullname);
-                rtndata.push({'type': 'object', 'fullpath': fullname});
-                //wn_walk_functions(rootnode[name], fullname);
-            }
-            
-        }
-        else if(typefound == "function"){
-            blacklistlookup = fullname.substring(5);
-            if (blacklist.includes(blacklistlookup) == false && name.startsWith("wn_") == false) {
-                rtndata.push({'type': 'function', 'fullpath': fullname});
-                console.log("FUNCTION: "+fullname+"()");
-            }
-        }
-        else {
-            rtndata.push({'type': 'ignored', 'fullpath': fullname});
-            //console.log("Name: "+name+" ["+typefound+"]"); 
-            //console.log(fullname)  
-        }
-    
-    }
-    
-    return rtndata;
-}
-"""
-        self.jsinjector.execute_javascript(self.driver, javascript)
+        self.jsinjector.install_custom_javascript_functions(self.driver)
 
         javascript="return wn_walk_functions(this, 'this');"        
         jsresults = self.executeJavascriptAndReturnArray(javascript)
